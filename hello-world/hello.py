8a411f97ff7953cbdd0b6d5c1202a04e0191b2fe
Ms_Ymg <ygoswell@2m-corp.com>
2024-07-18 15:16:52 +0000

    Update hello.py
    printing the variable names

--- hello-world/hello.py
+++ hello-world/hello.py
@@ -1,7 +1,10 @@
 print('Hello World!')
-a=1
-b=2
-c=a+b
-print('a =', a)
-print('b =', b)
-print('a plus b =', c)
+
+
+def sum(a, b):
+    c=a+b
+    print('a =', a)
+    print('b =', b)
+    print('a plus b =', c)
+
+sum(10,20)
